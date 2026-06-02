79c828b378a7d79921c70673b0c4b3e0fc7c90ae
NastyaSinitsyna <ren2507@yandex.ru>
2026-06-02 17:22:57 +0500

    "added NotFoundPage.jsx to components module"

---
+++ frontend/src/components/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+function NotFoundPage() {
+  return (
+    <>
+      <div className="container-fluid d-flex justify-content-center align-content-center h-100">
+        <div className="card shadow-sm text-center p-5">
+            <h1>404</h1>
+            <p>Страница не найдена</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default NotFoundPage
