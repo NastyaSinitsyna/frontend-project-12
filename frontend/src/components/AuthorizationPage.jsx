e75a3f9f229f6c5b82c159200288a07e7138694b
NastyaSinitsyna <ren2507@yandex.ru>
2026-06-03 13:16:41 +0500

    "added AuthorizationPage.jsx to components module"

---
+++ frontend/src/components/AuthorizationPage.jsx
@@ -0,0 +1,50 @@
+import { Formik, Form, Field } from 'formik'
+
+function AuthorizationPage() {
+  return (
+    <div className="container-fluid d-flex flex-column justify-content-center align-content-center h-100">
+      <div className="card shadow-sm">
+        <div className="card-body row p-5">
+          <Formik
+            initialValues={{ username: "", password: "" }}
+            onSubmit={() => {}}>
+              {() => (
+                <Form className="mt-3 mt-md-0">
+                  <h1 className="text-center mb-4">Войти</h1>
+                  <div className="form-floating mb-3">
+                    <Field
+                      id="username"
+                      type="text"
+                      name="username"
+                      placeholder="Ваш ник"
+                      className="form-control"
+                    />
+                    <label htmlFor="username">Ваш ник</label>
+                  </div>
+                  <div className="form-floating mb-3">
+                    <Field
+                      id="password"
+                      type="password"
+                      name="password"
+                      placeholder="Пароль"
+                      className="form-control"
+                    />
+                    <label htmlFor="password">Пароль</label>
+                  </div>
+                  <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
+                </Form>
+              )}
+          </Formik>
+        </div>
+        <div className="card-footer p-4">
+          <div className="text-center">
+            <span>Нет аккаунта?</span>
+            <a href="">Регистрация</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AuthorizationPage
