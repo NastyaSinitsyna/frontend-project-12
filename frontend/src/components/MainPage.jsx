b28d1539bcd8108410fc180246e0b7aab2e3bbe6
NastyaSinitsyna <ren2507@yandex.ru>
2026-06-03 13:17:11 +0500

    added MainPage.jsx to components module

---
+++ frontend/src/components/MainPage.jsx
@@ -0,0 +1,14 @@
+function MainPage() {
+  return (
+    <>
+      <div className="container-fluid d-flex justify-content-center align-content-center h-100">
+        <div className="card shadow-sm text-center p-5">
+            <h1>Основная страница</h1>
+            <p>Контент</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MainPage
