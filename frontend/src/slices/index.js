5828348dd84a043440d7db9aa67e3ecf00538337
NastyaSinitsyna <ren2507@yandex.ru>
2026-06-15 17:37:40 +0500

    added store config module

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from './authSlice.js'
+import channelsReducer from './channelsSlice.js'
+import messagesReducer from './messagesSlice.js'
+
 
 export default configureStore({
   reducer: {
     authData: authReducer,
+    channels: channelsReducer,
+    messages: messagesReducer,
   },
 })
