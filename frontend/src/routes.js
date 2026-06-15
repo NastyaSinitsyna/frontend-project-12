92f827e991284f9cc880964466f7ac5cb7590f37
NastyaSinitsyna <ren2507@yandex.ru>
2026-06-15 17:36:38 +0500

    edited routes module

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,7 @@
 const apiPath = '/api/v1'
 
 export default {
-  loginPath: () => [apiPath, 'login'].join('/')
+  loginPath: () => [apiPath, 'login'].join('/'),
+  channelsPath: () => [apiPath, 'channels'].join('/'),
+  messagesPath: () => [apiPath, 'messages'].join('/')
 }
