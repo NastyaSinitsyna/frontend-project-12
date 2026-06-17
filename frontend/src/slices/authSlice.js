fe57e57b964e9d92d70727ba7488060aac697041
NastyaSinitsyna <ren2507@yandex.ru>
2026-06-17 15:59:58 +0500

    edited auth Data by username usage in authSlice

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -8,7 +8,7 @@ export const getToken = createAsyncThunk(
   async (authValues, { rejectWithValue } ) => {
     try {
       const response = await axios.post(routes.loginPath(), authValues)
-      return response.data.token
+      return response.data
     } 
     catch(error) {
       return rejectWithValue({
@@ -20,9 +20,11 @@ export const getToken = createAsyncThunk(
 )
 
 const token = localStorage.getItem('token')
+const username = localStorage.getItem('username')
 
 const initialState = {
   token: token || null,
+  username: username || null,
   loggedIn: !!token,
   authFailed: false,
   error: null,
@@ -34,8 +36,10 @@ const authSlice = createSlice({
   reducers: {
     logOut: (state) => {
       state.token = null
+      state.username = null
       state.loggedIn = false
       localStorage.removeItem('token')
+      localStorage.removeItem('username')
     },
   },
   extraReducers: (builder) => {
@@ -44,11 +48,13 @@ const authSlice = createSlice({
       state.error = null
     })
     builder.addCase(getToken.fulfilled, (state, action) => {
-      state.token = action.payload
-      state.authFailed = false
+      state.token = action.payload.token
+      state.username = action.payload.username
       state.loggedIn = true
+      state.authFailed = false
       state.error = null
-      localStorage.setItem('token', action.payload)
+      localStorage.setItem('token', action.payload.token)
+      localStorage.setItem('username', action.payload.username)
     })
     builder.addCase(getToken.rejected, (state, action) => {
       state.error = action.payload
