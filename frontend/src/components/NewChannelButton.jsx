8c40aaae55e382dffcc29f6686decffcaf259424
NastyaSinitsyna <ren2507@yandex.ru>
2026-06-29 09:00:46 +0300

    added NewChannelButton.jsx module

---
+++ frontend/src/components/NewChannelButton.jsx
@@ -0,0 +1,39 @@
+import { Button, ButtonGroup, Dropdown } from 'react-bootstrap'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { setCurrentChannel } from '../slices/channelsSlice.js'
+import { toggleButtonVariant } from '../utilities.js'
+import RemoveModal from './RemoveModal.jsx'
+import RenameModal from './RenameModal.jsx'
+
+function NewChannelButton({ channel }) {
+  const dispatch = useDispatch()
+  const currentChannelId = useSelector(state => state.channels.currentChannelId)
+
+  const [showRemove, setShowRemove] = useState(false)
+  const [showRename, setShowRename] = useState(false)
+  
+  return (
+    <Dropdown as={ButtonGroup} className="w-100">
+      <Button
+        variant={toggleButtonVariant(channel.id, currentChannelId)}
+        className="flex-grow-1 text-start text-truncate"
+        onClick={() => dispatch(setCurrentChannel(channel.id))}>
+        <span className="me-1">#</span>
+        {channel.name}
+      </Button>
+      <Dropdown.Toggle split variant={toggleButtonVariant(channel.id, currentChannelId)} id="dropdown-split-basic" />
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={() => setShowRemove(true)}>Удалить</Dropdown.Item>
+        <Dropdown.Item onClick={() => setShowRename(true)}>Переименовать</Dropdown.Item>
+      </Dropdown.Menu>
+
+      <RemoveModal show={showRemove} onHide={() => setShowRemove(false)} channelId={channel.id} />
+      <RenameModal show={showRename} onHide={() => setShowRename(false)} channel={channel}/>
+    
+    </Dropdown>
+  )
+}
+
+export default NewChannelButton
