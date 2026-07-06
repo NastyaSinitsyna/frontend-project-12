8e0f10ca47f6dd522b9ee947b5726ba572e0d98f
NastyaSinitsyna <ren2507@yandex.ru>
2026-07-06 21:09:11 +0300

    added i18n Provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { I18nextProvider } from 'react-i18next'
+
+import i18nextInstance from './i18n.js'
 
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
-import App from './App.jsx'
+import App from './components/App.jsx'
 import store from './store/index.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <I18nextProvider i18n={i18nextInstance}>
+        <App />
+      </I18nextProvider>
     </Provider>
   </StrictMode>,
 )
