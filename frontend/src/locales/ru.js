44ce476048db50d2910f347c2fd6f17c9c39d7d1
NastyaSinitsyna <ren2507@yandex.ru>
2026-07-06 21:08:34 +0300

    added locales module

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,42 @@
+export default {
+  translation: {
+    actions: {
+      login: 'Войти',
+      logout: 'Выйти',
+      signup: 'Зарегистрироваться',
+      send: 'Отправить',
+      remove: 'Удалить',
+      rename: 'Переименовать',
+      cancel: 'Отменить',
+      addChannel: 'Добавить канал',
+      removeChannel: 'Удалить канал',
+      renameChannel: 'Переименовать канал',
+      confirmPassword: 'Подтвердите пароль',
+      typeMessage: 'Введите сообщение...'
+    },
+    errors: {
+      wrongUserData: 'Неверные имя пользователя или пароль',
+      userExists: 'Такой пользователь уже существует',
+      nameLength: 'От 3 до 20 символов',
+      passwordLength: 'Не менее 6 символов',
+      passwordMatch: 'Пароли должны совпадать',
+      required: 'Обязательное поле',
+      unique: 'Должно быть уникальным',
+    },
+    view: {
+      yourNickname: 'Ваш ник',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      noAccount: 'Нет аккаунта?',
+      signup: 'Регистрация',
+      channels: 'Каналы',
+      channelName: 'Имя канала',
+      newMessage: 'Новое сообщение',
+      message_one: '{{count}} сообщение',
+      message_few: '{{count}} сообщения',
+      message_many: '{{count}} сообщений',
+      confirmation: 'Уверены?',
+      notFoundPage: 'Страница не найдена',
+    },
+  }
+}
