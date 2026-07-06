1cc290a67696e4d55e8d4d65a424680088fc49ea
NastyaSinitsyna <ren2507@yandex.ru>
2026-07-06 21:13:02 +0300

    fixed NotFoundPage.jsx with i18n translations

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,10 +1,13 @@
+import { useTranslation } from "react-i18next"
+
 function NotFoundPage() {
+  const { t } = useTranslation()
   return (
     <>
       <div className="container-fluid d-flex justify-content-center align-content-center h-100">
         <div className="card shadow-sm text-center p-5">
             <h1>404</h1>
-            <p>Страница не найдена</p>
+            <p>{t('view.notFoundPage')}</p>
         </div>
       </div>
     </>
