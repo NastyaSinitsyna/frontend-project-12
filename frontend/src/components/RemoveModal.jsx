bea2865adcaba41adbe102e20f44b4f1e292ba00
NastyaSinitsyna <ren2507@yandex.ru>
2026-07-06 21:12:06 +0300

    fixed RemoveModal.jsx with i18n translations

--- frontend/src/components/RemoveModal.jsx
+++ frontend/src/components/RemoveModal.jsx
@@ -1,11 +1,11 @@
 import { Button, Modal } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-
+import { useTranslation } from 'react-i18next'
 import { removeChannel } from '../store/slices/channelsSlice.js'
 
 function RemoveModal({ show, onHide, channelId }) {
   const dispatch = useDispatch()
-
+  const { t } = useTranslation()
   const isLoading = useSelector(state => state.channels.isLoading)
 
   const handleRemove = () => {
@@ -17,17 +17,17 @@ function RemoveModal({ show, onHide, channelId }) {
     <>
       <Modal show={show} onHide={onHide}>          
         <Modal.Header closeButton>
-          <Modal.Title>Удалить канал</Modal.Title>
+          <Modal.Title>{t('actions.removeChannel')}</Modal.Title>
         </Modal.Header>
         <Modal.Body>       
           <div className="form-floating mb-3">
-            <p className="lead">Уверены?</p>
+            <p className="lead">{t('view.confirmation')}</p>
           </div>       
         </Modal.Body>
         <Modal.Footer>
           <div className="d-flex justify-content-end">
-            <Button type="button" variant="secondary" disabled={isLoading} onClick={onHide}>Отменить</Button>
-            <Button type="button" variant="danger" disabled={isLoading} onClick={handleRemove}>Удалить</Button>
+            <Button type="button" variant="secondary" disabled={isLoading} onClick={onHide}>{t('actions.cancel')}</Button>
+            <Button type="button" variant="danger" disabled={isLoading} onClick={handleRemove}>{t('actions.remove')}</Button>
           </div>
         </Modal.Footer>
     </Modal>
