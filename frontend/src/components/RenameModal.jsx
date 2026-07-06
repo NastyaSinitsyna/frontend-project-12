677c9c3ee742c87958eba4d3110d4548f151435a
NastyaSinitsyna <ren2507@yandex.ru>
2026-07-06 21:12:24 +0300

    fixed RenameModal.jsx with i18n translations

--- frontend/src/components/RenameModal.jsx
+++ frontend/src/components/RenameModal.jsx
@@ -2,12 +2,14 @@ import { Button, Form, Modal } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { useRef } from 'react' 
 import { useFormik } from 'formik'
+import { useTranslation } from 'react-i18next'
 import { validChannelSchema } from '../schemas/validChannelSchemas.js'
 
 import { editChannel, channelsSelectors } from '../store/slices/channelsSlice.js'
 
 function RenameModal({ show, onHide, channel }) {
   const dispatch = useDispatch()
+  const { t } = useTranslation()
   const channels = useSelector(channelsSelectors.selectAll)
   const inputRef = useRef(null)
   const isLoading = useSelector(state => state.channels.isLoading)
@@ -42,7 +44,7 @@ function RenameModal({ show, onHide, channel }) {
       <Modal show={show} onHide={handleClose} onEntered={() => inputRef.current?.select()}>
           <Form onSubmit={formik.handleSubmit}>
             <Modal.Header closeButton>
-              <Modal.Title>Переименовать канал</Modal.Title>
+              <Modal.Title>{t('actions.renameChannel')}</Modal.Title>
             </Modal.Header>
             <Modal.Body>       
               <div className="form-floating mb-3">
@@ -50,7 +52,7 @@ function RenameModal({ show, onHide, channel }) {
                   id="name"
                   type="text"
                   name="name"
-                  placeholder='Имя канала'
+                  placeholder={t('view.channelName')}
                   required
                   value={formik.values.name}
                   ref={inputRef}
@@ -59,14 +61,14 @@ function RenameModal({ show, onHide, channel }) {
                   onChange={handleChange}
                   onBlur={formik.handleBlur}
                 />
-                <Form.Label htmlFor="name">Имя канала</Form.Label>
+                <Form.Label htmlFor="name">{t('view.channelName')}</Form.Label>
                 <Form.Control.Feedback type="invalid">{formik.errors.name}</Form.Control.Feedback>
               </div>       
             </Modal.Body>
             <Modal.Footer>
               <div className="d-flex justify-content-end">
-                <Button type="button" variant="secondary" disabled={isLoading} onClick={handleClose}>Отменить</Button>
-                <Button type="submit" variant="primary" disabled={isLoading}>Отправить</Button>
+                <Button type="button" variant="secondary" disabled={isLoading} onClick={handleClose}>{t('actions.cancel')}</Button>
+                <Button type="submit" variant="primary" disabled={isLoading}>{t('actions.send')}</Button>
               </div>
             </Modal.Footer>
           </Form>
