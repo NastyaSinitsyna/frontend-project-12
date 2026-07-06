cf38fbb58bb7d8ccdaeba10b634bee1fb7c98424
NastyaSinitsyna <ren2507@yandex.ru>
2026-07-06 21:10:33 +0300

    fixed ChannelsPanel.jsx with i18n translations

--- frontend/src/components/ChannelsPanel.jsx
+++ frontend/src/components/ChannelsPanel.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useFormik } from 'formik'
 import { validChannelSchema } from '../schemas/validChannelSchemas.js'
+import { useTranslation } from 'react-i18next'
 
 import { addChannel, fetchChannels, setCurrentChannel, channelsSelectors } from '../store/slices/channelsSlice.js'
 
@@ -10,6 +11,7 @@ import ChannelsList from './ChannelsList.jsx'
 
 function ChannelsPanel() {
   const dispatch = useDispatch()
+  const { t } = useTranslation()
 
   const inputRef = useRef(null)
   const isLoading = useSelector(state => state.channels.isLoading)
@@ -47,14 +49,14 @@ function ChannelsPanel() {
   return (
     <Col className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-3">
-        <b>Каналы</b>
+        <b>{t('view.channels')}</b>
         <Button variant="primary" onClick={() => setShow(true)}>
           +
         </Button>
         <Modal show={show} onHide={handleClose} onEntered={() => inputRef.current?.select()}>
           <Form onSubmit={formik.handleSubmit}>
             <Modal.Header closeButton>
-              <Modal.Title>Добавить канал</Modal.Title>
+              <Modal.Title>{t('actions.addChannel')}</Modal.Title>
             </Modal.Header>
             <Modal.Body>       
               <div className="form-floating mb-3">
@@ -62,7 +64,7 @@ function ChannelsPanel() {
                   id="name"
                   type="text"
                   name="name"
-                  placeholder='Имя канала'
+                  placeholder={t('view.channelName')}
                   required
                   value={formik.values.name}
                   ref={inputRef}
@@ -71,14 +73,14 @@ function ChannelsPanel() {
                   onChange={handleChange}
                   onBlur={formik.handleBlur}
                 />
-                <Form.Label htmlFor="name">Имя канала</Form.Label>
+                <Form.Label htmlFor="name">{t('view.channelName')}</Form.Label>
                 <Form.Control.Feedback type="invalid">{formik.errors.name}</Form.Control.Feedback>
               </div>       
             </Modal.Body>
             <Modal.Footer>
               <div className="d-flex justify-content-end">
-                <Button type="button" variant="secondary" disabled={isLoading} onClick={handleClose}>Отменить</Button>
-                <Button type="submit" disabled={isLoading} variant="primary">Отправить</Button>
+                <Button type="button" variant="secondary" disabled={isLoading} onClick={handleClose}>{t('actions.cancel')}</Button>
+                <Button type="submit" disabled={isLoading} variant="primary">{t('actions.send')}</Button>
               </div>
             </Modal.Footer>
           </Form>
