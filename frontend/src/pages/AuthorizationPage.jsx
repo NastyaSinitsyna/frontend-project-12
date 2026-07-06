41e71e5b570baa44565a88ddad5ce432eda91729
NastyaSinitsyna <ren2507@yandex.ru>
2026-07-06 21:12:44 +0300

    fixed AuthorizationPage.jsx with i18n translations

--- frontend/src/pages/AuthorizationPage.jsx
+++ frontend/src/pages/AuthorizationPage.jsx
@@ -3,7 +3,7 @@ import { Button, Form } from 'react-bootstrap'
 import { Link, useNavigate, useLocation } from 'react-router-dom'
 import { useEffect, useRef } from 'react'
 import { useDispatch } from 'react-redux'
-
+import { useTranslation } from 'react-i18next'
 import { login } from '../store/slices/authSlice.js'
 import { logInSuccess } from '../store/slices/authSlice.js'
 
@@ -11,7 +11,7 @@ import { logInSuccess } from '../store/slices/authSlice.js'
 function AuthorizationPage() {
   const dispatch = useDispatch()
   const navigate = useNavigate()
-
+  const { t } = useTranslation()
   const location = useLocation()
   const inputRef = useRef()
   const redirectedPath = location.state?.from?.pathname ?? '/'
@@ -32,7 +32,7 @@ function AuthorizationPage() {
         if (error.status === 401) {
           formik.setStatus({
             authFailed: true,
-            message: 'Неверные имя пользователя или пароль'
+            message: t('errors.wrongUserData')
           })
         }
         else {
@@ -54,42 +54,42 @@ function AuthorizationPage() {
         <div className="card shadow-sm">
           <div className="card-body row p-5">
             <Form onSubmit={formik.handleSubmit} className="mt-3 mt-md-0">
-              <h1 className="text-center mb-4">Войти</h1>
+              <h1 className="text-center mb-4">{t('actions.login')}</h1>
               <div className="form-floating mb-3">
                 <Form.Control
                   id="username"
                   type="text"
                   name="username"
-                  placeholder="Ваш ник"
+                  placeholder={t('view.yourNickname')}
                   required
                   value={formik.values.username}
                   ref={inputRef}
                   isInvalid={formik.status?.authFailed}
                   onChange={handleChange}
                 />
-                <Form.Label htmlFor="username">Ваш ник</Form.Label>
+                <Form.Label htmlFor="username">{t('view.yourNickname')}</Form.Label>
               </div>
               <div className="form-floating mb-3">
                 <Form.Control
                   id="password"
                   type="password"
                   name="password"
-                  placeholder="Пароль"
+                  placeholder={t('view.password')}
                   required
                   value={formik.values.password}
                   isInvalid={formik.status?.authFailed}
                   onChange={handleChange}
                 />
-                <Form.Label htmlFor="password">Пароль</Form.Label>
+                <Form.Label htmlFor="password">{t('view.password')}</Form.Label>
                 <Form.Control.Feedback type="invalid">{formik.status?.message}</Form.Control.Feedback>
               </div>
-              <Button type="submit" disabled={formik.isSubmitting} className="w-100 mb-3 btn btn-primary">Войти</Button>
+              <Button type="submit" disabled={formik.isSubmitting} className="w-100 mb-3 btn btn-primary">{t('actions.login')}</Button>
             </Form>
           </div>
           <div className="card-footer p-4">
             <div className="text-center">
-              <span>Нет аккаунта?</span>
-              <Link to="/signup">Регистрация</Link>
+              <span>{t('view.noAccount')}</span>
+              <Link to="/signup">{t('view.signup')}</Link>
             </div>
           </div>
         </div>
