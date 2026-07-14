de5527a5e580fde97874ba7a23b34e41cee672c8
NastyaSinitsyna <ren2507@yandex.ru>
2026-07-15 00:12:56 +0300

    fixed logInSuccess process with storage change in AuthorizationPage.jsx

--- frontend/src/pages/AuthorizationPage.jsx
+++ frontend/src/pages/AuthorizationPage.jsx
@@ -2,10 +2,11 @@ import { useFormik } from 'formik'
 import { Button, Form } from 'react-bootstrap'
 import { Link, useNavigate, useLocation } from 'react-router-dom'
 import { useEffect, useRef } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import { login } from '../store/slices/authSlice.js'
 import { logInSuccess } from '../store/slices/authSlice.js'
+import { storage } from '../StorageService.js'
 import { toast } from 'react-toastify'
 
 
@@ -16,6 +17,8 @@ function AuthorizationPage() {
   const location = useLocation()
   const inputRef = useRef()
   const redirectedPath = location.state?.from?.pathname ?? '/'
+  const token = useSelector(state => state.authData.token)
+  const username = useSelector(state => state.authData.token)
 
   useEffect(() => {
     inputRef.current.focus()
@@ -27,6 +30,8 @@ function AuthorizationPage() {
       try {
         const loginResult = await dispatch(login(values)).unwrap()
         dispatch(logInSuccess(loginResult))
+        storage.setItem('token', token)
+        storage.setItem('username', username)
         navigate(redirectedPath, { replace: true })
       }
       catch (error) {
