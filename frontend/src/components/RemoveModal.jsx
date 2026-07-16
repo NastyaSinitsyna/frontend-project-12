2604b8dc6db0380e1c31b4b2c2aa70cc9b7647a6
NastyaSinitsyna <ren2507@yandex.ru>
2026-07-16 12:24:24 +0300

    fixed RemoveModal with modal reducers

--- frontend/src/components/RemoveModal.jsx
+++ frontend/src/components/RemoveModal.jsx
@@ -2,17 +2,20 @@ import { Button, Modal } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import { removeChannel } from '../store/slices/channelsSlice.js'
+import { hideModal } from '../store/slices/modalSlice.js'
 import { toast } from 'react-toastify'
 
-function RemoveModal({ show, onHide, channelId }) {
+
+function RemoveModal() {
   const dispatch = useDispatch()
   const { t } = useTranslation()
+  const { show, channel } = useSelector(state => state.modal)
   const isLoading = useSelector(state => state.channels.isLoading)
 
   const handleRemove = async () => {
     try {
-      await dispatch(removeChannel(channelId)).unwrap()
-      onHide()
+      await dispatch(removeChannel(channel.id)).unwrap()
+      dispatch(hideModal())
       toast.success(t('messages.channelRemoved'))
     }
     catch (error) {
@@ -23,7 +26,7 @@ function RemoveModal({ show, onHide, channelId }) {
   
   return (
     <>
-      <Modal show={show} onHide={onHide}>          
+      <Modal show={show} onHide={() => dispatch(hideModal())}>          
         <Modal.Header closeButton>
           <Modal.Title>{t('actions.removeChannel')}</Modal.Title>
         </Modal.Header>
@@ -34,7 +37,7 @@ function RemoveModal({ show, onHide, channelId }) {
         </Modal.Body>
         <Modal.Footer>
           <div className="d-flex justify-content-end">
-            <Button type="button" variant="secondary" disabled={isLoading} onClick={onHide}>{t('actions.cancel')}</Button>
+            <Button type="button" variant="secondary" disabled={isLoading} onClick={() => dispatch(hideModal())}>{t('actions.cancel')}</Button>
             <Button type="button" variant="danger" disabled={isLoading} onClick={handleRemove}>{t('actions.remove')}</Button>
           </div>
         </Modal.Footer>
